a1beba640c29e5e7041b0c7283cac5918ee68846
Mohammed Rizad Ibrahim M <mohammedrizadibrahimm24@gmail.com>
2024-03-24 19:46:41 +0530

    nineth commit

--- server/index.js
+++ server/index.js
@@ -11,6 +11,6 @@ const app = express();
 
 app.use(express.json());
 app.use('/', require('./routes/authRoutes'));
-const port = procees.env.PORT||8000;
+const port = process.env.PORT||8000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
